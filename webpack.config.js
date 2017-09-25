40f9941be25f269f4edad43d5d6229a3617e53f7
Hakim <kim.k866@gmail.com>
2017-09-26 00:32:36 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+
+    entry: './assets/js/app.js',
+    watch: true,
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'bundle.js'
+    }
+}
